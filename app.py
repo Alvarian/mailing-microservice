bc97b02e861bd0bd6762060ae6c011dd58ad4153
Alvarian <alvarivan88@gmail.com>
2022-07-10 19:13:08 -0400

    lock pipfile, reattempt deployment

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ import os
 import json
 
 import smtplib
-import atexit
 from threading import Timer
 
 from email.message import EmailMessage
 from flask_cors import CORS
+import flask_cors
+# flask_cors.cross_origin(["http://www.domain1.com"]) 
+
 from flask import Flask, request
 app = Flask(__name__)
 CORS(app)
 
-EMAIL_ADDRESS = os.environ.get('EMAIL_U');
-EMAIL_PASSWORD = os.environ.get('EMAIL_P');
+EMAIL_ADDRESS = os.environ.get('EMAIL_U')
+EMAIL_PASSWORD = os.environ.get('EMAIL_P')
 
 state = []
 
@@ -20,8 +22,9 @@ state = []
 @app.route("/api/v1/mail/message", methods=['post'])
 def post_message():
 	payload = request.get_json()
-
 	msg = EmailMessage()
+
+
 	msg['Subject'] = 'New message from my Portfolio'
 	msg['From'] = payload['guestEmail']
 	msg['To'] = EMAIL_ADDRESS
@@ -49,17 +52,34 @@ def post_message():
 @app.route("/api/v1/mail/complaints", methods=['post'])
 def post_complains():
 	payload = request.get_json()
+	msg = EmailMessage()
 
-	print(not len(state), len(state))
 	def timeout_trigger_email(*ref_state):
-		with open('complaints_payload.json', 'w') as f:
+		date = ref_state[0]['error'] and ''.join(e for e in ref_state[0]['error']['time'] if e.isalnum()) or len(ref_state[0]['warnings']) and ''.join(e for e in ref_state[0]['warnings'][0]['time'] if e.isalnum())
+		filename = 'complaint_report_'+date+'.json'
+
+		with open(filename, 'w') as f:
 			json.dump(ref_state, f, indent=2)
-			state.clear()
-			print("New json file is created from data.json file", state)
+
+		with open(filename, 'r') as readable_f:
+			msg['Subject'] = 'NOTICE: Portfolio found '+str(len(ref_state))+' logs...'
+			msg['From'] = 'norely_from_portfolio@gmail.com'
+			msg['To'] = EMAIL_ADDRESS
+
+			content = readable_f.read()
+			
+			msg.add_attachment(content, filename=filename)
+
+		with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+			smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+			smtp.send_message(msg)
+
+
+		state.clear()
+		print("New json file is created from data.json file", state)
 
 	# push payload in state. if state is empty, set timer for 24 hours to send state as stringified notification
 	if not len(state):
-		print("trigger called")
 		state.append(payload)
 
 		s = Timer(2.0, timeout_trigger_email, (state))
