f1309e411234ba3ed07365db386147457052a843
Alvarian <vitarka88@gmail.com>
2022-07-10 15:38:59 -0400

    add new endpoint to store payload messages in json file, staged to send ready file to email when timeout is satisfied

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import os
-import smtplib
-from email.message import EmailMessage
+import json
 
+import smtplib
+import atexit
+from threading import Timer
+
+from email.message import EmailMessage
 from flask_cors import CORS
 from flask import Flask, request
 app = Flask(__name__)
 CORS(app)
 
+EMAIL_ADDRESS = os.environ.get('EMAIL_U');
+EMAIL_PASSWORD = os.environ.get('EMAIL_P');
 
-@app.route("/api/v1/mail", methods=['post'])
-def index():
-	EMAIL_ADDRESS = os.environ.get('EMAIL_U');
-	EMAIL_PASSWORD = os.environ.get('EMAIL_P');
+state = []
 
+
+@app.route("/api/v1/mail/message", methods=['post'])
+def post_message():
 	payload = request.get_json()
-	
+
 	msg = EmailMessage()
 	msg['Subject'] = 'New message from my Portfolio'
 	msg['From'] = payload['guestEmail']
@@ -39,6 +45,31 @@ def index():
 		smtp.send_message(msg)
 
 	return "Ok"
+
+@app.route("/api/v1/mail/complaints", methods=['post'])
+def post_complains():
+	payload = request.get_json()
+
+	print(not len(state), len(state))
+	def timeout_trigger_email(*ref_state):
+		with open('complaints_payload.json', 'w') as f:
+			json.dump(ref_state, f, indent=2)
+			state.clear()
+			print("New json file is created from data.json file", state)
+
+	# push payload in state. if state is empty, set timer for 24 hours to send state as stringified notification
+	if not len(state):
+		print("trigger called")
+		state.append(payload)
+
+		s = Timer(2.0, timeout_trigger_email, (state))
+
+		s.start()
+	else:
+		state.append(payload)
+
+	return "Ok"
+	
 
 
 if __name__ == '__main__':
